3e01697db2133c1d227ac14238246d45f049696e
Mehak Walia <waliamehakjeet@gmail.com>
2026-01-01 03:58:32 -0800

    setup MongoDB connection and data models

---
+++ internal/database/mongo.go
@@ -0,0 +1,30 @@
+package database
+
+import (
+	"context"
+	"log"
+	"time"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+var DB *mongo.Database
+
+func ConnectDB(uri string) error {
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	client, err := mongo.Connect(ctx, options.Client().ApplyURI(uri))
+	if err != nil {
+		return err
+	}
+
+	if err := client.Ping(ctx, nil); err != nil {
+		return err
+	}
+
+	DB = client.Database("attendance")
+	log.Println("MongoDB connected successfully")
+	return nil
+}
---
+++ internal/models/user.go
@@ -0,0 +1,13 @@
+package models
+
+import (
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+type User struct {
+	ID       primitive.ObjectID `bson:"_id,omitempty" json:"_id,omitempty"`
+	Name     string             `bson:"name" json:"name"`
+	Email    string             `bson:"email" json:"email"`
+	Password string             `bson:"password" json:"-"`
+	Role     string             `bson:"role" json:"role"`
+}
